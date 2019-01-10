832eed341b40ae4176220a1781e3e39490575194
MaslovRG <al.maslovrg@gmail.com>
2019-01-10 14:59:51 +0300

    Add code coverage exclude

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -10,9 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Gateway
 {
+    [ExcludeFromCodeCoverage]
     public class Startup
     {
         public Startup(IConfiguration configuration)
