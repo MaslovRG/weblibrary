987d926bce06e4546470c4186ee7e56126aec513
MaslovRG <al.maslovrg@gmail.com>
2019-01-10 14:26:52 +0300

    Add worked gateway

--- Gateway/Controllers/GatewayController.cs
+++ Gateway/Controllers/GatewayController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Gateway.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class GatewayController : ControllerBase
     {
