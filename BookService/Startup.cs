5445df98b70ec83c5c9a055141268ffd0eb8ff89
MaslovRG <al.maslovrg@gmail.com>
2019-01-15 19:02:13 +0300

    Microfix ")"

--- BookService/Startup.cs
+++ BookService/Startup.cs
@@ -29,7 +29,7 @@ namespace BookService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BooksContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionDB")); 
+            services.AddDbContext<BooksContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionDB"))); 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
